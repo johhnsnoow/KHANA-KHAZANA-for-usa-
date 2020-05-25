3c6c4201dd4250d57960193fdcf2efaa9929dec9
johhnsnoow <johhnsnoow@gmail.com>
2020-05-25 22:02:20 +0530

    Updates

---
+++ precache-manifest.4c2aeef4bb32068096d53627997144e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10834d1f0bb0169e20cdeeb2e840adf9",
+    "url": "/KHANA-KHAZANA-for-usa-/index.html"
+  },
+  {
+    "revision": "c229032126eb78f24261",
+    "url": "/KHANA-KHAZANA-for-usa-/static/css/main.c8d76b69.chunk.css"
+  },
+  {
+    "revision": "5a366a90119cd72cb8f9",
+    "url": "/KHANA-KHAZANA-for-usa-/static/js/2.15bfb385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KHANA-KHAZANA-for-usa-/static/js/2.15bfb385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c229032126eb78f24261",
+    "url": "/KHANA-KHAZANA-for-usa-/static/js/main.580ddb43.chunk.js"
+  },
+  {
+    "revision": "944e380d5927337db011",
+    "url": "/KHANA-KHAZANA-for-usa-/static/js/runtime-main.410041ba.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/KHANA-KHAZANA-for-usa-/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/KHANA-KHAZANA-for-usa-/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
